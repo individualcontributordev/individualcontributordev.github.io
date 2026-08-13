9627baa50fedb55dac6e1b2fc8aefe8b72283522
individualcontributordev <contributorindividual@gmail.com>
2026-08-13 23:34:12 +0100

    builder: rank single-disc-on-csr-v0.1.34 break auto after core

--- builder/builder.js
+++ builder/builder.js
@@ -1770,6 +1770,7 @@ async function onFileChosen(file) {
 		if (
 			id.includes('single-disc-on-csr-ref-') ||
 			id.includes('single-disc-on-csr-v0.1.26') ||
+			id.includes('single-disc-on-csr-v0.1.34') ||
 			id.includes('path-engine')
 		) {
 			return 21;
